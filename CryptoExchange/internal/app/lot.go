00e98721ce6fa27900f5614433f9bbbc462bb8c5
Dashiiidzzze <ejikevpsihiy@gmail.com>
2024-11-25 17:33:32 +0700

    исправлены ошибки в получении информации о лотах

--- CryptoExchange/internal/app/lot.go
+++ CryptoExchange/internal/app/lot.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
@@ -16,12 +15,10 @@ func HandleGetLot(w http.ResponseWriter, r *http.Request) {
 	var reqBD string = "SELECT * FROM lot"
 
 	response, err := RquestDataBase(reqBD)
-	if err == nil {
+	if err != nil {
 		return
 	}
-	strResponse := string(response)
 
-	fmt.Println(strResponse)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(strResponse)
+	json.NewEncoder(w).Encode(response)
 }
